9ee9fa55ab31017b9cbcb746a529535071a6397d
hsikelias <plekishsai@gmail.com>
2025-08-04 12:53:57 -0400

    Added a simple string chessboard with the black and white file ranks

---
+++ Chess/ChessEngine.py
@@ -0,0 +1,20 @@
+"""
+This is responsible for storing all the info about the current state of a chess game. It will also be responsible
+for determining the valid moves. Also keeps the move log.
+"""
+
+class GameState():
+    def __init__(self):
+        # Board design
+        self.board = [
+            ["bR","bN","bB","bQ","bK","bB","bN","bR"], #back rank pieces for black pov
+            ["bp","bp","bp","bp","bp","bp","bp","bp"], #black pawn rank row
+             # Representing a blank space on the board?
+            ["--","--","--","--","--","--","--","--"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            # could js leave em blank or 0, but this makes it easier to implement some core features in the future.
+            ["wR", "wN", "wB", "wQ", "wK", "wB", "wN", "wR"],  # white rank pieces for black pov
+            ["wp", "wp", "wp", "wp", "wp", "wp", "wp", "wp"],  # white pawn rank row
+        ]
---
+++ Chess/ChessMain.py
@@ -0,0 +1,3 @@
+"""
+This is the main driver file, responsible for handling user input and displaying the real time GameState
+"""
